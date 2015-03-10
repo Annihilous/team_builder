bd71cc15c5d0e21401ce90874f9462bb3fa5e1cf
Annihilous <benjaminjnorton@gmail.com>
2015-03-10 11:31:30 -0400

    first commit

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,48 @@
+class MembersController < ApplicationController
+  before_action :all_members, only: [:index, :create, :update, :destroy]
+  before_action :set_member, only: [:show, :edit, :update, :destroy]
+
+  # GET /members
+  # GET /members.json
+
+def ajax
+end
+
+  # GET /members/new
+  def new
+    @member = Member.new
+  end
+
+  # POST /members
+  # POST /members.json
+  def create
+    @member = Member.create(member_params)
+  end
+
+  # PATCH/PUT /members/1
+  # PATCH/PUT /members/1.json
+  def update
+    @member.update(member_params)
+  end
+
+  # DELETE /members/1
+  # DELETE /members/1.json
+  def destroy
+    @member.destroy
+  end
+
+  private
+    def all_members
+      @members = Member.all
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_member
+      @member = Member.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def member_params
+      params.require(:member).permit(:name, :team, :leader)
+    end
+end
---
+++ app/views/members/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @member, :id, :name, :team, :leader, :created_at, :updated_at
